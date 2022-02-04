5e991469321294459bfdc9610b84747f4e79f191
andreypotkas <potkasa@gmail.com>
2022-02-04 22:42:30 +0300

    add loginUser, createUser, getUser in file api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,4 +1,5 @@
 import Word from '../entity/word';
+import { Authorization } from '../authorization/authorization';
 
 const baseUrl = 'https://rs-lang-application.herokuapp.com';
 const words = `${baseUrl}/words`;
@@ -18,7 +19,62 @@ export async function createWord(userId: string, wordId: string): Promise<void> 
   });
   return response.json();
 }
+///////////////////////////////////////////////////////
+interface IUser{
+  name?: string,
+  email: string,
+  password: string
+}
+interface IAuthorizedUser{
+  token:string,
+  userId:string
+}
+// registration User in Data base
+export async function createUser(body: IUser):Promise<void>{
+  const user = await fetch(`${baseUrl}/users`, {
+      method: 'POST',
+      body:JSON.stringify(body),
+      headers:{
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+  });
+  const content = await user.json();
+  console.log(content);
+}
 
-// add updateWord method
-// add markWordAsDifficult method (should use updateWord)
-// add markWordAsLearned method (should use updateWord)
+const currentUser={
+  token:'',
+  userId:''
+}
+// log user 
+export const loginUser = async function (user:IUser): Promise<void> {
+  const response = await fetch(`${baseUrl}/signin`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  });
+  const content = await response.json();
+  console.log(content);
+  localStorage.setItem('token', content.token);
+  localStorage.setItem('userId', content.userId);
+  currentUser.token = content.token;
+  currentUser.userId = content.userId;
+  console.log(currentUser);
+};
+
+async function getUser(id:string, token:string) {
+  const word = await fetch(`${baseUrl}/users/${id}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+    }
+  });
+  return word.json();
+}
+const authorization = new Authorization()
+
