31b14040f7e739312e1a8ae100dd0b04fe4d3fdf
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2022-02-07 22:26:38 +0300

    add Difficult Words section

--- src/components/bookPage.ts
+++ src/components/bookPage.ts
@@ -1,46 +1,52 @@
 /* eslint-disable no-underscore-dangle */
 import { Word } from '../service/interfaces';
-import { getWords, getAggregatedWords } from '../service/api';
+import { getWords, getAggregatedWords, getDifficultWords } from '../service/api';
 import { isUserLoggedIn } from '../utils/loginUtils';
 
 class BookPage {
   pageNumber: number;
   groupNumber: number;
+  isUserLoggedIn: boolean;
 
   constructor(groupNumber: number, pageNumber: number) {
     this.pageNumber = pageNumber;
     this.groupNumber = groupNumber;
+    this.isUserLoggedIn = isUserLoggedIn();
   }
 
   async getWords(): Promise<Word[]> {
-    return isUserLoggedIn()
+    return (this.isUserLoggedIn)
       ? getAggregatedWords(this.groupNumber, this.pageNumber)
       : getWords(this.groupNumber, this.pageNumber);
   }
 
-  async render(): Promise<void> {
+  async render(isOnlyDifficultWords = false): Promise<void> {
     const container = document.getElementById('content') as HTMLDivElement;
-    const words = await this.getWords();
-    const wordsHtml = words.map((word) => BookPage.createWordCard(word)).join('');
+    const words = isOnlyDifficultWords ? await getDifficultWords() : await this.getWords();
+    const wordsHtml = words.map((word) => this.createWordCard(word)).join('');
     container.innerHTML = wordsHtml;
   }
 
-  static createWordCard(word: Word): string {
+  createWordCard(word: Word): string {
     let isDifficult = '';
+    let btnText = 'mark difficult';
     const isLearnt = '';
 
     if (word.userWord) {
-      isDifficult = word.userWord.difficulty;
+      if (word.userWord.difficulty === 'difficult') {
+        isDifficult = 'difficult';
+        btnText = 'unmark difficult';
+      }
       // isLearnt = (word.userWord.isLearnt) ? 'learnt' : '';
     }
 
-    const isHidden = (isUserLoggedIn()) ? '' : 'hidden';
+    const isHidden = (this.isUserLoggedIn) ? '' : 'hidden';
     return `
       <div class="wordCard ${isDifficult} ${isLearnt}" data-wordId=${word._id || word.id}>
         <div>Word: ${word.word}</div>
         <div>Translation: ${word.wordTranslate}</div>
         <div>Transcription: ${word.transcription}</div>
-        <button class="difficultBtn" ${isHidden}>difficult</button>
+        <button class="difficultBtn" ${isHidden}>${btnText}</button>
         <button class="learntBtn" ${isHidden}>learnt</button>
       </div>
     `;
