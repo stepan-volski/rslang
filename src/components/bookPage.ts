aae5a215f7fab4e0384f3706b50ea8d507cf4351
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2022-02-14 20:54:51 +0300

    wordCard style
    add word card style
    add fonts

--- src/components/bookPage.ts
+++ src/components/bookPage.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+import { baseUrl } from '../service/api';
 import { Word } from '../service/interfaces';
 import { getAggregatedWords, getDifficultWords } from '../service/usersWordsApi';
 import { getWords } from '../service/wordsApi';
@@ -50,12 +51,30 @@ class BookPage {
     const isHidden = (this.isUserLoggedIn) ? '' : 'hidden';
     return `
       <div class="wordCard ${isDifficult} ${isLearnt}" data-wordId=${word._id || word.id}>
-        <div>Word: ${word.word}</div>
-        <div>Translation: ${word.wordTranslate}</div>
-        <div>Transcription: ${word.transcription}</div>
-        <button class="difficultBtn" ${isHidden}>${diffBtnText}</button>
-        <button class="learntBtn" ${isHidden}>${learnBtnText}</button>
-        <div ${isHidden}>Word Game Score: ${wordGameScore}</div>
+        <img class="cardImage" src="${baseUrl}/${word.image}"></img>
+        <div class="cardContent">
+          <div class="cardHeader">
+            <div class="subHeader">
+              <span>${word.word}</span>
+              <span>${word.transcription}</span>
+              <span><img class="cardAudioIcon" src="../assets/audio_icon.png"></img></span>
+            </div>
+            <div>${word.wordTranslate}</div>
+          </div>
+
+          <div class="cardBody">
+            <div>${word.textExample}</div>
+            <div class="subSentence">${word.textExampleTranslate}</div>
+          </div>
+          <div class="cardBody">
+            <div>${word.textMeaning}</div>
+            <div class="subSentence">${word.textMeaningTranslate}</div>
+          </div>
+
+          <button class="difficultBtn" ${isHidden}>${diffBtnText}</button>
+          <button class="learntBtn" ${isHidden}>${learnBtnText}</button>
+          <div ${isHidden}>Word Game Score: ${wordGameScore}</div>
+        </div>
       </div>
     `;
   }
