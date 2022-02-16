cedf8ab2b3138cf96d0bc07e7f5de089b78c88c9
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2022-02-16 19:49:02 +0300

    add games end and start screens
    add game end screen
    add game start screen

--- src/pages/games.ts
+++ src/pages/games.ts
@@ -1,10 +1,15 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable import/no-cycle */    // TODO REMOVE!!!
 /* eslint-disable class-methods-use-this */   // TODO REMOVE!!!
 import { launchGameFromGames } from '../utils/challengeUtils';
 import Page from './abstract/page';
 
 class Games extends Page {
+  selectedGame: string;
+
   constructor() {
     super('Games');
+    this.selectedGame = '';
   }
 
   openPage(): void {
@@ -15,37 +20,60 @@ class Games extends Page {
   renderPageElements(): void {
     const appContainer = document.getElementById('app') as HTMLElement;
     const pageHtml = `
-    <div id="GamesContainer">
-      <button id="audioChallengeBtn" data-game="challenge">Audio Challenge</button>
-      <button id="sprintBtn" data-game="sprint">Sprint</button>
-      <select id="groupSelect">
-      <option value="0">1</option>
-      <option value="1">2</option>
-      <option value="2">3</option>
-      <option value="3">4</option>
-      <option value="4">5</option>
-      <option value="5">6</option>
-    </select>
-    </div>
+
+      <div class="gamesPageContainer">
+        <div class="gameBtn">
+          <img class="gameLogo" src="../assets/game_sprint.png"></img>
+          <div>Sprint</div>
+          <button data-game="sprint">play</button>
+        </div>
+        <div class="gameBtn" data-gameType="challenge">
+          <img class="gameLogo" src="../assets/game_challenge.png"></img>
+          <div>Audio Challenge</div>
+          <button data-gameType="challenge">play</button>
+        </div>
+      </div>
+
   `;
     (appContainer as HTMLElement).innerHTML = pageHtml;
   }
 
   initHandlers(): void {
+    document.addEventListener('click', this.startGame.bind(this));
     document.addEventListener('click', this.launchGame.bind(this));
   }
 
-  async launchGame(event: Event): Promise<void> {
+  async startGame(event: Event): Promise<void> {
     const element = event.target as HTMLElement;
-    const gameName = element.dataset.game;
-    const group = Number((document.getElementById('groupSelect') as HTMLSelectElement).value);
+    const group = element.dataset.group;
 
-    if (gameName === 'challenge') {
-      launchGameFromGames(group, 'challenge');
+    if (group) {
+      launchGameFromGames(Number(group), this.selectedGame);
     }
+  }
 
-    if (gameName === 'sprint') {
-      launchGameFromGames(group, 'sprint');
+  launchGame(event: Event): void {
+    const element = event.target as HTMLElement;
+    const gameName = element.dataset.gametype;
+    if (gameName) {
+      this.selectedGame = gameName;
+
+      const appContainer = document.getElementById('app') as HTMLElement;
+      const pageHtml = `
+      <div class="gamesPageContainer">
+        <h2>Select the level:</h2>
+        <div class="levelSelector">
+          <div class="groupCircle" data-group="0">1</div>
+          <div class="groupCircle" data-group="1">2</div>
+          <div class="groupCircle" data-group="2">3</div>
+          <div class="groupCircle" data-group="3">4</div>
+          <div class="groupCircle" data-group="4">5</div>
+          <div class="groupCircle" data-group="5">6</div>
+        </div>
+      </div>
+
+    `;
+      (appContainer as HTMLElement).innerHTML = pageHtml;
     }
   }
 }
