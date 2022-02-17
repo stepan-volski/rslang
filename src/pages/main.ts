87b93e4b586cac1fabd98f14e2953db22dce6672
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2022-02-18 00:55:26 +0300

    main page style
    add main page style
    add Games page style
    change menu icons size

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -8,8 +8,37 @@ class Main extends Page {
 
   openPage(): void {
     const pageName = this.name;
-    const appContainer = document.getElementById('app') as HTMLElement;
-    appContainer.innerHTML = `<div id="${pageName.toLowerCase()}Container">${pageName} container is here</div>`;
+
+    const pageHtml = `
+    <div class="mainPageContainer">
+      <h1>Welcome to RS Lang!</h1>
+      <div>
+        This app allows students to learn new English words and track their progress using the following instruments.
+      </div>
+
+      <h2>Games</h2>
+      <div>
+        Games allow you to learn new words in a fun and challenging way.
+        Select a difficulty level or use the page from the textbook as a source of words.
+      </div>
+
+      <h2>Textbook</h2>
+      <div>
+        This section allows you too see multiple words divided into 6 levels according to their difficulty.
+        Logged users received access to game statistics for each word card, as well as the possibility for
+        marking each word as Learned or Difficult.
+      </div>
+
+      <h2>Statistics</h2>
+      <div>
+        Track you progress using the Statistics section, which gives you the access to you daily amount of
+        learned words and game victories, as well as long term display of your overall progression.
+        The section is available for logged users only!
+      </div>
+
+    </div>`;
+
+    this.appContainer.innerHTML = pageHtml;
     addPageTitle(this.name);
   }
 }
